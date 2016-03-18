3729c00c3799dca82a5aa6feb6b2f42f66c6d863
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-18 16:56:13 +0000

    Game can be played

--- spec/battleship/game_spec.clj
+++ spec/battleship/game_spec.clj
@@ -19,12 +19,45 @@
  :cruiser :hit
  :destroyer :untouched}
 
+(def ships
+  {:submarine #{:A1}})
+
+(def destroyer-ships
+  {:destroyer #{:B1 :B2}})
 (describe "BattleWomanShip"
           (it "Sinks ship if all co-ordinates for that ship are guessed"
-              (should= {:submarine :sunk} (to-collate [[:A1 :hit :submarine]] ships)))
+              (should= {:submarine :sunk} (statuses #{:A1} ships)))
           (it "Marks ship as untouched"
-              (should= {:submarine :untouched} (to-collate [[:A2 :miss]] ships)))
+              (should= {:submarine :untouched} (statuses #{:A2} ships)))
+          (it "Marks ship as hit"
+              (should= {:destroyer :hit} (statuses #{:B2} destroyer-ships)))
+          (it "Sinks destroyer"
+              (should= {:destroyer :sunk} (statuses #{:B1 :B2} destroyer-ships)))
+          (it "Marks multiple ships"
+              (should= {:submarine :sunk
+                        :destroyer :untouched} (statuses #{:A1} {:submarine #{:A1} :destroyer #{:B1 :B2}})))
           (it "Marks ship as hit"
               (should= [:A1 :hit :submarine] (to-hit :A1 ships)))
           (it "does not mark when no ship is hit"
               (should= [:A2 :miss] (to-hit :A2 ships))))
+
+(describe :play-move
+          (it "plays a first move"
+              (should= [{:submarine :sunk} #{:A1}] (play-guess :A1 #{} ships)))
+          (it "remembers guesses"
+              (should= [{:destroyer :sunk} #{:B2 :B1}] (play-guess :B2 #{:B1} destroyer-ships))))
+
+(describe :game-over
+          (it "is not over is nothing hit"
+              (should= false (game-over? {:a :untouched :b :untouched})))
+          (it "is over"
+              (should= true (game-over? {:a :sunk :b :sunk}))))
+
+(describe :play-game
+          (it "Formats ship status"
+              (should= "Destroyer untouched!\nSubmarine sunk!\n" (format-ships {:submarine :sunk
+                                                                          :destroyer :untouched})) )
+          (it "plays until won"
+               (should-contain "Destroyer sunk!\nSubmarine sunk!\n\nGame over, well done!"
+                               (with-out-str (with-in-str "A1\nA2\nB1\n"
+                                               (play-game {:submarine #{:B1} :destroyer #{:A1 :A2}} #{}))))))
--- src/battleship/game.clj
+++ src/battleship/game.clj
@@ -1,7 +1,6 @@
-(ns battleship.game)
-
-(def ships
-  {:submarine #{:A1}})
+(ns battleship.game
+  (:require [clojure.set :as sets])
+ (:gen-class) )
 
 (defn- ship-contains-coord? [guess [ship-name coordinates]]
   (when (coordinates guess) ship-name))
@@ -14,15 +13,43 @@
     [guess :hit shipname]
     [guess :miss]))
 
-(defn- no-hits? [hits coordinates]
- (empty? (clojure.set/intersection (set (map first hits)) coordinates)))
+(defn- no-hits? [coordinates hit-set]
+  (empty? (sets/intersection hit-set coordinates)))
 
-; map each submarine to either sunk, hit or untouched
-(defn collate-ship [hits [ship-name coordinates]]
- (if (no-hits? hits coordinates)
-  {ship-name :untouched}
-  {ship-name :sunk})
-  )
+(def all-hits? sets/subset?)
 
-(defn to-collate [hits ships]
+(defn collate-ship [hit-set [ship-name coordinates]]
+  (cond
+    (no-hits? coordinates hit-set) {ship-name :untouched}
+    (all-hits? coordinates hit-set) {ship-name :sunk}
+    :else {ship-name :hit}))
+
+(defn statuses [hits ships]
   (apply merge (map (partial collate-ship hits) ships)))
+
+(defn play-guess [guess previous-guesses ships]
+  (let [guesses (conj previous-guesses guess)]
+    [(statuses guesses ships) guesses]))
+
+(defn game-over? [statuses]
+  (= '(:sunk) (distinct (map val statuses))))
+
+(defn format-ship [[ship-name status]]
+  (str (clojure.string/capitalize (name ship-name)) " " (name status) "!\n"))
+
+(defn format-ships [statuses]
+  (apply str (map format-ship (sort-by first statuses))))
+
+(defn play-game [ships previous-guesses]
+ (let [guess (keyword (read-line))
+       statuses (play-guess guess previous-guesses ships)]
+   (println (format-ships (first statuses)))
+
+   (if (game-over? (first statuses))
+     (println "Game over, well done!")
+     (play-game ships (second statuses))
+     )
+   ))
+
+(defn -main []
+  (play-game {:submarine #{:A1} :destroyer #{:B1 :B2}} #{}))
