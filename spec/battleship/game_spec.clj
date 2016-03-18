4496876e959218d9ab6d147cc2b09bd07c313cda
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-18 15:48:08 +0000

    A ship can be hit or untouched

--- spec/battleship/game_spec.clj
+++ spec/battleship/game_spec.clj
@@ -11,7 +11,20 @@
 
 ;(is-sunk? ) ?
 
+[:A1 :hit :submarine]
+[:A2 :hit :submarine]
+[:A3 :hit :cruiser]
+
+{:submarine :sunk
+ :cruiser :hit
+ :destroyer :untouched}
+
 (describe "BattleWomanShip"
-          (it "Guessed co-ordinate hits a ship"
-              (should= true
-                       (hit? :A1 ships))))
+          (it "Sinks ship if all co-ordinates for that ship are guessed"
+              (should= {:submarine :sunk} (to-collate [[:A1 :hit :submarine]] ships)))
+          (it "Marks ship as untouched"
+              (should= {:submarine :untouched} (to-collate [[:A2 :miss]] ships)))
+          (it "Marks ship as hit"
+              (should= [:A1 :hit :submarine] (to-hit :A1 ships)))
+          (it "does not mark when no ship is hit"
+              (should= [:A2 :miss] (to-hit :A2 ships))))
--- src/battleship/game.clj
+++ src/battleship/game.clj
@@ -3,5 +3,26 @@
 (def ships
   {:submarine #{:A1}})
 
-(defn hit? [guess ships]
-  (boolean (some true? (map (fn [[ship-name coordinates]] (boolean (some #(= guess %) coordinates))) ships))))
+(defn- ship-contains-coord? [guess [ship-name coordinates]]
+  (when (coordinates guess) ship-name))
+
+(defn- find-ship [guess ships]
+  (some (partial ship-contains-coord? guess) ships))
+
+(defn to-hit [guess ships]
+  (if-let [shipname (find-ship guess ships)]
+    [guess :hit shipname]
+    [guess :miss]))
+
+(defn- no-hits? [hits coordinates]
+ (empty? (clojure.set/intersection (set (map first hits)) coordinates)))
+
+; map each submarine to either sunk, hit or untouched
+(defn collate-ship [hits [ship-name coordinates]]
+ (if (no-hits? hits coordinates)
+  {ship-name :untouched}
+  {ship-name :sunk})
+  )
+
+(defn to-collate [hits ships]
+  (apply merge (map (partial collate-ship hits) ships)))
